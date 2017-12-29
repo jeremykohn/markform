d8c8ccd33c5c9542d248a1b1fc7ad2c4b96db335
Jeremy Kohn <jeremykohn@users.noreply.github.com>
2017-12-29 01:34:32 -0500

    Rewrite as two functions: 1. divide line, 2. parse tab

--- parse_element.py
+++ parse_element.py
@@ -9,6 +9,8 @@
 
 # "parse_element()", "parse_line()", "split_element()", or "split_line()"
 
+
+# Or, 'get indices of pre, tag, and post'?
 def parse_element(line):
 
     # Validate input.
@@ -21,15 +23,32 @@ def parse_element(line):
         raise ValueError("Markdown element cannot include newline")
     
     # Components to return.
+    tag_type = None
     pre_tag_text = ""
     tag_inner_content = ""
     post_tag_text = ""
-    tag_type = None 
 
-    # Conditions to satisfy to create a Markform tag.
-    tag_open = False
-    token = None
-    tag_closed = False
+    # Initial conditions.
+    tag_open = False  # tag_open requires opening bracket followed by opening token.  # Guess I don't need this.
+    tag_closed = False  # tag_closed requires closing token followed by closing bracket.  # Or this.
+    opening_token = None
+    closing_token = None    
+
+    # Types of Markform tokens.
+    # Move these to class variable?
+    
+    # For some types of Markform tags, the opening and closing tokens are the same character.
+    simple_tokens = ['+', '-', '_', '@', '$', '%', '^', '*']
+        
+    # For other Markform tags, the closing token is the inverse of the opening token.
+    inverse_tokens = {
+        "(": ")",
+        "[": "]",            
+        "{": "}",
+        "|": "|"
+    }
+    
+    # Ready.
     
     # Current position. Start at beginning of line.
     pos = 0
@@ -50,131 +69,127 @@ def parse_element(line):
             
             
     # Get content after initial spaces, before opening bracket.
-    while pos < len(line): 
+
+    
+    # Search for opening bracket followed by opening token.
+    # If found, get opening token and (based on opening token) closing token.
+    while pos + 1 < len(line):
         if line[pos] == "[":
-            tag_open = True
-        else:
-            pre_tag_text += line[pos]
-        # Step forward.
+            next_character = line[pos + 1]
+            if next_character in simple_tokens:
+                opening_token = next_character
+                closing_token = opening_token
+                break
+            elif next_character in inverse_tokens:
+                opening_token = next_character
+                closing_token = inverse_tokens[opening_token]
+                break
+        # If not found, keep moving.
         pos += 1
 
-    # If there is an opening bracket, get the next character, if any.
-    # That character is a "token" that determines the type of Markform tag.
-    if tag_open:
-        if pos + 1 < len(line):
-            token = line[pos + 1]
-    else:
-        token = None
-    
-    
+    if opening_token
+
     # If there is an opening token, search for a tag closing, 
     # which is a closing token followed by a closing bracket.
-    if token:
+    if opening_token:
+        pos_left = pos
+        pos_right = pos
+        # Move pos_right to find closing bracket preceded by closing token(s?)
+        while pos_right + 1 < len(line):
+            if line[pos_right] == closing_token and line[pos_right + 1] == "]":
+                # Found tag closing.
+                left_bracket_index = pos_left
+                right_bracket_index = pos_right + 1
+                break
+       
+    if left_bracket_index and right_bracket_index:
+        # Divide string and return each.
+        # Or, return indices and let other function divide string.
+        pre_tag_text = line[:left_bracket_index]
+        tag_text = line[left_bracket_index:right_bracket_index+1]
+        post_tag_text = line[right_bracket_index+1:]
+        return (pre_tag_text, tag_text, post_tag_text)
+            # Our work here is done.
+            # Other methods will parse tag itself, get token, get inner content, 
+            # combine label with inner content to create HTMl tags, etc.
+    else:
+        return None
 
-        # For some types of Markform tags, the opening and closing tokens are the same character.
-        single_tokens = ['+', '-', '_', '@', '$', '%', '^', '*']
+
+                
+
+def parse_tag(tag_text):
+    # Return token type --> tag type, and also inner text.
+    
+    # Sanity checks
+    
+    if type(tag_text) is not str:
+        raise ValueError("Tag must be a string.")
+    
+    if tag_text = "":
+        raise ValueError("Tag text cannot be empty.")
+
+    # Other validations? Confirm tag format?
+    # Should be bracket, opening token, ends with corresponding closing token followed by bracket.
+    # Might want to confirm that before proceeding.
+    
+    # Meanwhile, get the token type.
+    
+    pos_left = 0
+    pos_right = len(tag_text) - 1
+
+    # Move inward, get tag type, and inner content if any.
+    
+    # If not the right format, or token not on list, return error. (ValueError?) (Tag must be in the right format.)
+    
+    pos_left += 1
+    opening_token = tag_text[pos_left]
+    
+    if opening_token in simple_tokens:
+        closing_token = 
+        while tag_text[pos_left] == opening_token:
+            # Advance pos_left
+            # After opening tokens (and after whitespace?), inner content begins.
+            # (I think yes after whitespace. Otherwise [_inner content_] might look like emphasis, underlines, etc.
+            pos_left += 1
+        # Now, skip whiespace.
+        while tag_text[pos_left] == " ":
+            pos_left += 1
+        # At this point, 
+        # pos_left should be at first character of inner content.
+        # Or else at closing bracket.
         
-        # For other Markform tags, the closing token is the inverse of the opening token.
-        # This dictionary maps each opening token to the corresponding closing token.
-        inverse_tokens = {
-            "(": ")",
-            "[": "]",            
-            "{": "}",
-            "|": "|"
-        }
-
+        # If pos_left == pos_right, it means pos_left is at closing bracket.
+        # If not, then move pos_right leftward to find end of inner content.
         
-        # See if token is on one of the relevant lists.    
+        # Although, if no closing token, something is wrong -- throw error.
 
+    elif opening_token in inverse_tokens:
+        closing_token = inverse_tokens[opening_token]
+        # No multiple opening tokens allowed if the tag has inverse tokens.
+        # So, just advance one position.
+        # Then skip whitespace.
         
-        
-        
+    
+    
+    # In either case,
+        # get pos_left to the beginning of inner content,
+        # and pos_right to the end of inner content.
+        # That is, if there's inner content... which there isn't if pos_left and pos_right are the same.
 
-        
-        if token in single_tokens:
-            pos_left = pos
-            pos_right = pos
-            # Search forward for closing bracket.
-            while pos_right < len(line):
-                pos_right += 1
-                if line[pos_right] == "]":
-                    pos_left_bracket = pos_left
-                    pos_right_bracket = pos_right
-                    pos_left_token = pos_left + 1
-                    pos_right_token = pos_right - 1
-                    # Now, look for closing token.
-                    if line[pos_right - 1] == token:
-                        tag_closed = True
-                        break
-                    # Or, reuse this for closing token?
-            
-            
-            # Get content in between continous opening token(s) and continuous closing token(s).
-            # Work forwards, and backwards.
-
-            # (Again, change j and i to more descriptive variable names)
-            if tag_closed:
-                i += 1
-                j -= 1
-                while line[i] == token and i < j:
-                    i += 1
-                    # At this point, line[i] should be the first post-token character, if any.
-                while line[j] == token and i < j:
-                    j -= 1
-                    # At this point, line[j] should be the last pre-token character, if any.
-                if i <= j:
-                    # There is inner content.
-                    # One or more characters.
-                    inner_content = line[i:j+1]
-                else:
-                    inner_content = None
-
-                # Make sure to test this! Avoid off-by-one errors.
-
-        elif token in inverse_tokens:
-            
-            # Get the opening token and the corresponding closing token.
-            opening_token = token            
-            closing_token = inverse_tokens[opening_token]
-            
-            # Now, parse for closing token.
-            # If found, get tag type (based on opening token) and inner content.
-            # and set tag_closed = True
-            
-            j = i
-            while j < len(line):
-                j += 1
-                if line[j] == ']' and line[j-1] == closing_token:
-                    # Found end of tag.
-                    tag_closed = True
-                    break            
-
-            # Now, get inner content between tokens.
-            
             # For symmetric tokens, multiple opening/closing tokens are not allowed.
             # Instead, get content between (first) opening token and (first) closing token.
             # For example: `[[[ ]]]`
             # That would become `<textarea>[</textarea>]`
             # Assuming of course that a space is not required after opening token.
+            # And assuming we allow pre tag text or post tag text to border tag.
             
-            if tag_closed:
-                i += 1  # First post-opening-token character
-                j -= 2  # Last pre-closing-token character
-                if i >= j:
-                    # Inner content is everything between opening token and closing token
-                    inner_content = line[i:j+1]
 
-        else:
-            # Token not on list. Can't close tag.
-            tag_closed = False
-
-    if tag_open and token and tag_closed:
+            
         # Return tag type, and inner content.
         # Tag type depends on token.
-        print("Tag opened and closed. Token: " + token)
+        print("Token: " + token)
 
-        # if inner_content,
-        # return that too.
         
     else:
         return None
