f6f9c3734163c0a380242e7cb04d764aa4d81718
Jeremy Kohn <jeremykohn@users.noreply.github.com>
2018-01-18 00:36:04 -0500

    Refactor for clarity

--- convert_document.py
+++ convert_document.py
@@ -6,25 +6,30 @@ def convert_document(self, document_text):
 
     for line_number, line in enumerate(lines):
 
-        # If line includes Markform element, returns element type. Else, returns None.
+        # If line includes Markform element, returns element type. 
+        # Else, returns None.
         element_type = get_element_type(line)
-            
+
         if element_type == "start" and not form_open:
             # Open a Markform block.
             form_open = True
             # Append newline, to separate converted HTML from previous content.
             document_output += '\n'
-            
-        if element_type and form_open:
-            # Append converted Markform element.
+            # Convert and append Markform start element.
             document_output += convert_line_to_html(line)
-        
-        if element_type == "end" and form_open:
-            # Close the Markform block.
-            form_open = False            
+
+        elif element_type == "end" and form_open:
+            # Convert and append Markform end element.
+            document_output += convert_line_to_html(line)
             # Append newline, to separate converted HTML from successive content.
             document_output += '\n'
-        
+            # Close the Markform block.
+            form_open = False
+
+        elif element_type and form_open:
+            # Append converted Markform element.
+            document_output += convert_line_to_html(line)
+
         else:
             # If earlier conditions not met, don't convert line, just append unconverted line.
             document_output += line            
