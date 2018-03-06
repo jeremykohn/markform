5f0118ba9aba73fda0076f4953ef93d917c26c8f
Jeremy Kohn <jeremykohn@users.noreply.github.com>
2018-03-06 00:48:25 -0500

    refactor: Rename functions and variables, change function arguments
    The variable element_type is now called tag_type. 
    
    The function to convert a line to HTML is renamed from convert_line_to_html, which takes an unparsed line as input, to convert_tag_to_html, which takes a parsed line as input. This way, the line need not be parsed twice.

--- convert_document.py
+++ convert_document.py
@@ -6,29 +6,29 @@ def convert_document(self, document_text):
 
     for line_number, line in enumerate(lines):
 
-        # If line includes Markform element, returns element type. 
-        # Else, returns None.
-        element_type = get_element_type(line)
+        # Parse line to extract relevant Markform information, if any.
+        # Returns None for tag_type if line does not include a Markform tag.
+        tag_type, pre_tag_content, inner_content, post_tag_content = parse_line(line)
 
-        if element_type == "start" and not form_open:
+        if tag_type == "start" and not form_open:
             # Open a Markform block.
             form_open = True
             # Append newline, to separate converted HTML from previous content.
             document_output += '\n'
             # Convert and append Markform start element.
-            document_output += convert_line_to_html(line)
+            document_output += convert_tag_to_html(tag_type, pre_tag_content, inner_content, post_tag_content)
 
-        elif element_type == "end" and form_open:
+        elif tag_type == "end" and form_open:
             # Convert and append Markform end element.
-            document_output += convert_line_to_html(line)
+            document_output += convert_tag_to_html(tag_type, pre_tag_content, inner_content, post_tag_content)
             # Append newline, to separate converted HTML from successive content.
             document_output += '\n'
             # Close the Markform block.
             form_open = False
 
-        elif element_type and form_open:
+        elif tag_type and form_open:
             # Inside Markform block. Convert and append Markform element.
-            document_output += convert_line_to_html(line)
+            document_output += convert_tag_to_html(tag_type, pre_tag_content, inner_content, post_tag_content)
 
         else:
             # If earlier conditions not met, don't convert line, just append unconverted line.
