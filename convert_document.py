109022cdb53ff89ead43e87fe2c41a51af58a356
Jeremy Kohn <jeremykohn@users.noreply.github.com>
2018-03-06 01:49:16 -0500

    refactor: convert_tag_to_html now returns tag text, not just inner content

--- convert_document.py
+++ convert_document.py
@@ -8,7 +8,7 @@ def convert_document(self, document_text):
 
         # Parse line to extract relevant Markform information, if any.
         # Returns None for tag_type if line does not include a Markform tag.
-        tag_type, pre_tag_content, inner_content, post_tag_content = parse_line(line)
+        tag_type, tag_text, pre_tag_text, post_tag_text = parse_line(line)
 
         if tag_type == "start" and not form_open:
             # Open a Markform block.
@@ -16,11 +16,11 @@ def convert_document(self, document_text):
             # Append newline, to separate converted HTML from previous content.
             document_output += '\n'
             # Convert and append Markform start element.
-            document_output += convert_tag_to_html(tag_type, pre_tag_content, inner_content, post_tag_content)
+            document_output += convert_tag_to_html(tag_type, tag_text, pre_tag_text, post_tag_text)
 
         elif tag_type == "end" and form_open:
             # Convert and append Markform end element.
-            document_output += convert_tag_to_html(tag_type, pre_tag_content, inner_content, post_tag_content)
+            document_output += convert_tag_to_html(tag_type, tag_text, pre_tag_text, post_tag_text)
             # Append newline, to separate converted HTML from successive content.
             document_output += '\n'
             # Close the Markform block.
@@ -28,7 +28,7 @@ def convert_document(self, document_text):
 
         elif tag_type and form_open:
             # Inside Markform block. Convert and append Markform element.
-            document_output += convert_tag_to_html(tag_type, pre_tag_content, inner_content, post_tag_content)
+            document_output += convert_tag_to_html(tag_type, tag_text, pre_tag_text, post_tag_text)
 
         else:
             # If earlier conditions not met, don't convert line, just append unconverted line.
