caa2f4d87b86af01ee98130fb76d968440ce6570
Jeremy Kohn <jeremykohn@users.noreply.github.com>
2018-04-09 18:44:35 -0500

    refactor: Rename variable for consistency
    Rename markform_type to markform_element_type and detect_markform_type to detect_element_type

--- convert_markform_document_to_html.py
+++ convert_markform_document_to_html.py
@@ -4,10 +4,10 @@ def convert_markform_document_to_html(markform_document):
     output_lines = []
 
     for line in input_lines:
-        markform_type = detect_markform_type(line)
-        if markform_type:
+        markform_element_type = detect_element_type(line)
+        if markform_element_type:
             # Convert and append.
-            converted_line = convert_markform_line(line, markform_type)
+            converted_line = convert_markform_line(line, markform_element_type)
             output_lines.append(converted_line)
         else:
             # Don't convert, just append.
