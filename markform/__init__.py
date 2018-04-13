03a3ccf6e585700c2456ec1f4b94fce5053f06ed
Jeremy Kohn <jeremykohn@users.noreply.github.com>
2018-04-12 23:59:09 -0500

    refactor: Create separate scripts for converting text, converting file

--- markform/__init__.py
+++ markform/__init__.py
@@ -1,2 +1,3 @@
-from converters import convert_text, convert_file
+from converters.convert_text import convert_text
+from converters.convert_file import convert_file
 __all__ = ['convert_text', 'convert_file']
